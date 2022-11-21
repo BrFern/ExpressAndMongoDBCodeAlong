f3eb900dd215f72b1f4edb450144add30a79d16c
BrFern <102929013+BrFern@users.noreply.github.com>
2022-11-21 10:21:52 -0700

    Adding after server, dotenv, and express were initialized at 13:33

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('WHy wont you commit')
+//Adding express and dotenv
+
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+//Adding a port
+const port = process.env.PORT || 5000 //allows you to access the port variable in .env
+
+
+const app = express()
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
+
