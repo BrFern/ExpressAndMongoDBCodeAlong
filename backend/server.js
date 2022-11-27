b9de885be1ae6a9a0acc420ddcd17591fbf372c6
BrFern <102929013+BrFern@users.noreply.github.com>
2022-11-27 11:59:00 -0700

    Was having more internet issues, unable to finish last ten minutes of video to push to Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+//Deploying
+
 //Adding express and dotenv
 const path = require('path');
 const express = require('express');
@@ -18,6 +20,14 @@ app.use(express.urlencoded({extended: false}));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+//Serve frontend
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'../', 
+    'frontend', 'build', 'index.html')))
+}
 
 app.use(errorHandler);
 
